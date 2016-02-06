1ffed502317369e2609f786dedb7bf4e235d117d
Matthew Andrews <matt@mattandre.ws>
2016-02-06 11:01:50 +0000

    Simpler version of express-errors-handler

---
+++ main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+if (process.env.NODE_ENV === 'production') {
+	var client = new raven.Client(process.env.RAVEN_URL);
+	module.exports = client;
+
+	// Die on uncaughtException
+	// https://github.com/getsentry/raven-node#catching-global-errors
+	client.patchGlobal(function() {
+		process.exit(1);
+	});
+} else {
+	module.exports = {
+		captureMessage: console.warn.bind(logger),
+		captureError: console.error.bind(logger)
+	};
+}
